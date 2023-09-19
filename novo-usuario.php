18d9fd2ff65ff12f8131c357bf60a685522059b1
Magno F.Nóbrega <magnofnobrega@outlook.com>
2023-09-19 18:56:36 -0300

    curso crud php

---
+++ novo-usuario.php
@@ -0,0 +1,25 @@
+<h1>Novo Usuário</h1>
+<form action="">
+    <div class="mb-3">
+        <label for="">Nome</label>
+        <input type="text" name="nome" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label for="">E-mail</label>
+        <input type="email" name="email" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label for="">Senha</label>
+        <input type="password" name="senha" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label for="">Data de Nascimento</label>
+        <input type="date" name="data_nasc" class="form-control">
+    </div>
+    <div class="mb-3">
+        <button type="submit" class="btn btn-primary">
+            Enviar
+        </button>
+    </div>
+
+</form>
